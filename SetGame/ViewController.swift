c422d0efbd9650dfb96e1146caf949f6e231621a
Owen Delisle <owen37164@gmail.com>
2020-02-23 17:13:39 -0800

    Initial Commit

---
+++ SetGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SetGame
+//
+//  Created by Owen Delisle on 2020-02-23.
+//  Copyright © 2020 Owen Delisle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
